d9431e7362817aebe28fdd3aeb40024b9ddac045
Raven <RavensKrag@gmail.com>
2014-07-24 03:35:41 -0400

    Move main library code into proper file, and executing code into main.rb

---
+++ bin/main.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+path_to_file = File.expand_path(File.dirname(__FILE__))
+Dir.chdir path_to_file do
+	require '../lib/object-packer'
+end
+
+x = Foo.new('./source/', '')
+x.run
