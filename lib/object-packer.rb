d565d0854951c151b22b97e25b13fa4ec8845f2c
Raven <RavensKrag@gmail.com>
2014-07-25 06:05:32 -0400

    Bring over discriptions of line transforms.

--- lib/object-packer.rb
+++ lib/object-packer.rb
@@ -280,22 +280,31 @@ class << self
 		line.strip_comment
 	end
 	
+	# x = a --> a = x
 	def reverse_assignment(line, header_data)
 		line.split('=').collect{ |i| i.strip }.reverse.join(' = ')
 	end
 	
+	# format: Class.new arg1, arg2, ..., argn = var
+	# result: arg = var.arg
 	def extraction_from_initialization(line, header_data)
 		line
 	end
 	
+	# OBJECT is the thing being examined
+	# this replaces it with a reference to self
+	# but the replacement is not on the OBJECT tag,
+	# but on variables within the BODY text that have the same name
 	def replace_object_with_self(line, header_data)
 		line
 	end
 	
+	# blank out lines with bang commands
 	def process_bang_command_with_arguments(line, header_data)
 		line
 	end
 	
+	# blank out lines with bang commands
 	def ignore_bang_commands(line, header_data)
 		line
 	end
