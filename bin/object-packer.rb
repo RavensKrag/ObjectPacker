48f39284a6d5f41226fa156efc7d6f006f3777f3
Raven <RavensKrag@gmail.com>
2014-07-25 14:32:59 -0400

    Load config file in executable, and pass command line arguments to Rake.

--- bin/object-packer.rb
+++ bin/object-packer.rb
@@ -1,29 +1,26 @@
 #!/usr/bin/env ruby
 
 require 'rubygems'
-require 'rake'
+
+require 'yaml'
+
+
+
+
+
+puts "execution directory: #{Dir.pwd}"
+
+config_filepath = File.join('.', 'object-packer.config.yml')
+data = YAML.load_file config_filepath
+
+source_dir = File.absolute_path data[:source_directory]
+output_dir = File.absolute_path data[:output_directory]
+
+
+
 
 
 path_to_file = File.expand_path(File.dirname(__FILE__))
-
-
-
-# code below on how to load and run a rakefile through ruby code is taken from stackexchange
-# src: http://stackoverflow.com/questions/3530/how-do-i-rake-tasks-within-a-ruby-script
-
-app = Rake.application
-app.init
-
-
 Dir.chdir path_to_file do
-path_to_rakefile = File.join('.', 'Rakefile')
-app.add_import path_to_rakefile
+	system "rake source_dir='#{source_dir}' output_dir='#{output_dir}'"
 end
-
-app.load_rakefile
-
-
-
-puts Dir.pwd
-
-app['default'].invoke
--- lib/object-packer/version.rb
+++ lib/object-packer/version.rb
@@ -1,3 +1,3 @@
 module ObjectPacker
-	VERSION = '0.00.3'
+	VERSION = '0.00.4'
 end
